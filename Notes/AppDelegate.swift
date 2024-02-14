66d3dcacf8136583b2e407065f83ff4769474c29
Aleksandra Nikiforova <alexnikxx@gmail.com>
2024-02-14 16:30:37 +0100

    addes logs using CocoaLumberjack

--- Notes/AppDelegate.swift
+++ Notes/AppDelegate.swift
@@ -14,17 +14,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         DDLog.add(DDOSLogger.sharedInstance) // Uses os_log
-        
-        let fileLogger: DDFileLogger = DDFileLogger() // File Logger
-        fileLogger.rollingFrequency = 60 * 60 * 24 // 24 hours
-        fileLogger.logFileManager.maximumNumberOfLogFiles = 7
-        DDLog.add(fileLogger)
-        
-        DDLogError("This is an error message")
-        DDLogWarn("This is a warning message")
-        DDLogInfo("This is an info message")
-        DDLogDebug("This is a debug message")
-        
+        DDLogDebug("Function: \(#function)")
+
         return true
     }
     
@@ -33,6 +24,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
         // Called when a new scene session is being created.
         // Use this method to select a configuration to create the new scene with.
+        DDLogDebug("Function: \(#function)")
+
         return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
     }
     
@@ -40,6 +33,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // Called when the user discards a scene session.
         // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
         // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
+        
+        DDLogDebug("Function: \(#function)")
     }
     
     
