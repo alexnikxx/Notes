c6678b9c88f51ca5a0519a369e6dc1268f43e84e
Aleksandra Nikiforova <alexnikxx@gmail.com>
2024-05-29 17:39:10 +0200

    Implement table View

--- Notes/ListOfNotes/FirstTabViewController.swift
+++ Notes/ListOfNotes/FirstTabViewController.swift
@@ -8,8 +8,52 @@
 import UIKit
 
 final class FirstTabViewController: UIViewController {
+    let notes = Note.allNotes
+
+    private var tableView: UITableView = {
+        let tableView = UITableView()
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        return tableView
+    }()
+
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .white
+        view.addSubview(tableView)
+
+        setupView()
+    }
+
+    private func setupView() {
+        NSLayoutConstraint.activate([
+            tableView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            tableView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor),
+            tableView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor),
+            tableView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor)
+        ])
+
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+    }
+}
+
+extension FirstTabViewController: UITableViewDataSource, UITableViewDelegate {
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return notes.count
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell(style: .subtitle, reuseIdentifier: nil)
+        let note = notes[indexPath.row]
+        cell.textLabel?.text = note.title
+        cell.detailTextLabel?.text = note.content
+        return cell
+    }
+
+    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        return "Prototype Cells"
     }
 }
--- Notes/Models/Note.swift
+++ Notes/Models/Note.swift
@@ -16,15 +16,15 @@ struct Note: Codable {
     let color: UIColor
     let importance: Importance
     let selfDestructionDate: Date?
-    
+
     enum Importance: Int, Decodable {
         case important = 1
         case normal = 2
         case unimportant = 3
     }
-    
+
     private enum CodingKeys: String, CodingKey { case uid, title, content, color, importance, selfDestructionDate }
-    
+
     init(uid: String?, title: String, content: String, color: UIColor?, importance: Importance, selfDestructionDate: Date?) {
         self.uid = uid ?? UUID().uuidString
         self.title = title
@@ -33,7 +33,7 @@ struct Note: Codable {
         self.importance = importance
         self.selfDestructionDate = selfDestructionDate ?? nil
     }
-    
+
     init(from decoder: Decoder) throws {
         let container = try decoder.container(keyedBy: CodingKeys.self)
         uid = try container.decode(String.self, forKey: .uid)
@@ -43,7 +43,7 @@ struct Note: Codable {
         importance = try container.decode(Importance.self, forKey: .importance)
         selfDestructionDate = try? container.decode(Date.self, forKey: .selfDestructionDate)
     }
-    
+
     public func encode(to encoder: Encoder) throws {
         var container = encoder.container(keyedBy: CodingKeys.self)
         try container.encode(uid, forKey: .uid)
@@ -58,13 +58,62 @@ struct Note: Codable {
 extension Note {
     static var allNotes: [Note] {
         return [
-            Note(uid: nil, title: "Заголовок заметки", content: "Текст заметки, Текст заметки, Текст заметки, Текст заметки, Текст заметки", color: UIColor.red, importance: Importance.normal, selfDestructionDate: nil),
-            Note(uid: nil, title: "Короткая заметка", content: "Привет", color: UIColor.green, importance: Importance.normal, selfDestructionDate: nil),
-            Note(uid: nil, title: "Длинная заметка", content: "Очень длинная заметка, Очень длинная заметка, Очень длинная заметка, Очень длинная заметка, Очень длинная заметка, Очень длинная заметка, Очень длинная заметка, Очень длинная заметка, Очень длинная заметка, Очень длинная заметка, Очень длинная заметка", color: UIColor.blue, importance: Importance.normal, selfDestructionDate: nil),
-            Note(uid: nil, title: "Заголовок заметки - 2", content: "Текст заметки, Текст заметки, Текст заметки, Текст заметки, Текст заметки", color: UIColor.yellow, importance: Importance.normal, selfDestructionDate: nil),
-            Note(uid: nil, title: "Короткая заметка", content: "Не забыть выключить утюг", color: UIColor.cyan, importance: Importance.normal, selfDestructionDate: nil),
-            Note(uid: nil, title: "Короткая заметка", content: "Не забыть выключить утюг", color: UIColor.cyan, importance: Importance.normal, selfDestructionDate: nil),
-            Note(uid: nil, title: "Короткая заметка", content: "Не забыть выключить утюг", color: UIColor.cyan, importance: Importance.normal, selfDestructionDate: nil)
+            Note(
+                uid: nil,
+                title: "Заголовок заметки",
+                content: "Текст заметки, Текст заметки, Текст заметки, Текст заметки, Текст заметки",
+                color: UIColor.red,
+                importance: Importance.normal,
+                selfDestructionDate: nil
+            ),
+            Note(
+                uid: nil,
+                title: "Короткая заметка",
+                content: "Привет",
+                color: UIColor.green,
+                importance: Importance.normal,
+                selfDestructionDate: nil
+            ),
+            Note(
+                uid: nil,
+                title: "Длинная заметка",
+                content: "Очень длинная заметка, Очень длинная заметка, Очень длинная заметка, Очень длинная заметка, Очень длинная заметка, Очень длинная заметка, Очень длинная заметка, Очень длинная заметка, Очень длинная заметка, Очень длинная заметка, Очень длинная заметка",
+                color: UIColor.blue,
+                importance: Importance.normal,
+                selfDestructionDate: nil
+            ),
+            Note(
+                uid: nil,
+                title: "Заголовок заметки - 2",
+                content: "Текст заметки, Текст заметки, Текст заметки, Текст заметки, Текст заметки",
+                color: UIColor.yellow,
+                importance: Importance.normal,
+                selfDestructionDate: nil
+            ),
+            Note(
+                uid: nil,
+                title: "Короткая заметка",
+                content: "Не забыть выключить утюг",
+                color: UIColor.cyan,
+                importance: Importance.normal,
+                selfDestructionDate: nil
+            ),
+            Note(
+                uid: nil,
+                title: "Короткая заметка",
+                content: "Не забыть выключить утюг",
+                color: UIColor.cyan,
+                importance: Importance.normal,
+                selfDestructionDate: nil
+            ),
+            Note(
+                uid: nil,
+                title: "Короткая заметка",
+                content: "Не забыть выключить утюг",
+                color: UIColor.cyan,
+                importance: Importance.normal,
+                selfDestructionDate: nil
+            )
         ]
     }
 }
